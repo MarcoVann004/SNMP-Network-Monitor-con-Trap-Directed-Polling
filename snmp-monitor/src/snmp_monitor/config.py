786f2c8d3af6e005b0b22fc76780187b10e55b71
Riccardo Pionati <riccardopionati@iMac-di-Ivan.station>
2026-07-01 17:11:37 +0200

    Bug fixes

--- snmp-monitor/src/snmp_monitor/config.py
+++ snmp-monitor/src/snmp_monitor/config.py
@@ -7,12 +7,12 @@ def leggi_config(path: str) -> list[AgentConfig]:
     Legge il contenuto yaml del file di configurazione e restituisce una lista di oggetti AgentConfig.
     """
 
-    with open(path, "r") as f:
-        agent = yaml.safe_load(f)
+    with open(path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
     
     agents = []
     
-    for agent in agent["agents"]:
+    for agent in config["agents"]:
         agent = AgentConfig(
             name = agent["name"],
             host = agent["host"],
